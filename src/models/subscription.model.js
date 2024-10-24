e38e2ec2e60d7728df5950c823ed446ccc512bda
Yogindra <yogindrachaudhari816@gmail.com>
2024-10-24 21:44:05 +0530

    typo fixed in subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,16 +1,19 @@
-import mongoose, {Schema} from 'mongoose'
+import mongoose, { Schema } from "mongoose";
 
-new subscriptionSchema = new Schema({
+const subscriptionSchema = new Schema(
+  {
     subscriber: {
-        type: Schema.Types.ObjectId,
-        // One who is subscribing
-        ref: "User"
+      type: Schema.Types.ObjectId,
+      // One who is subscribing
+      ref: "User",
     },
     channel: {
-        type: Schema.Types.ObjectId,
-        // One to whom 'subscriber' is subscribing
-        ref: "User"
-    }
-}, {timestamps: true});
+      type: Schema.Types.ObjectId,
+      // One to whom 'subscriber' is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Subscription = mongoose.model("Subscription", subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
